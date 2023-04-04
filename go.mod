bff72c8f0bd938f010adac56adcd682698c1a72d
BuildTools <altamiranolucas2013@gmail.com>
2023-04-04 18:12:35 +0200

    NixTon

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Lucas104-alt/NixTon
 
 go 1.20
+
+require github.com/google/uuid v1.3.0 // indirect
--- main.go
+++ main.go
@@ -1,6 +1,16 @@
-package Main
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+)
 
 func main() {
-	Nixton := "Nixton 1.0.0"
-	println(Nixton)
+	nixton := "NixTon 1.0.0 "
+	fmt.Print(nixton)
+	fmt.Print("Creado Por Lucas11 ")
+	fmt.Println(" UUID:", uuid.New().String())
+	log.Print()
 }
